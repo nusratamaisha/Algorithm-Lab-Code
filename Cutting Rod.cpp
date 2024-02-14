34632dca8428c1986cea52f795c23ee6182cda87
nusratamaisha <110341885+nusratamaisha@users.noreply.github.com>
2024-02-15 00:43:31 +0600

    Update Cutting Rod.cpp

--- Cutting Rod.cpp
+++ Cutting Rod.cpp
@@ -1,30 +1,32 @@
 #include<iostream>
 using namespace std;
-int cutting_rod(int price[],int length){
-    int dp[length+1][length+1];
-    int i,j;
-    for(i=0;i<=length; i++){
-        dp[i][0] = 0;
-    }
-    for(j=1;j<=length; j++){
-        dp[0][j] = 0;
-    }
-    for(i=1; i<=length;i++){
-        for(j=1; j<=length; j++){
-            if (i <= j) {
-                dp[i][j] = max(dp[i][j], price[i] + dp[i][j - i]); // Update the maximum value for length i and cut j
-            }
-            dp[i][j] = max(dp[i][j], dp[i - 1][j]);
+
+int cutting_rod(int price[], int length) {
+    int dp[length + 1];
+    dp[0] = 0; // Base case
+
+    // Bottom-up DP approach
+    for (int i = 1; i <= length; i++) {
+        int maxProfit = -1; // Initialize maximum profit for current length i
+        for (int j = 0; j < i; j++) {
+            maxProfit = max(maxProfit, price[j] + dp[i - j - 1]); // Consider all possible cuts for length i
         }
+        dp[i] = maxProfit; // Store the maximum profit for length i
     }
-    return dp[length][length];
+    return dp[length]; // Return the maximum profit for the entire rod
 }
-int main(){
-    int length,i;
-    cin>>length;
+
+int main() {
+    int length;
+    cout << "Enter the length of the rod: ";
+    cin >> length;
+
     int price[length];
-    for(i=0; i<length; i++){
-        cin>>price[i];
+    cout << "Enter the price for each length from 1 to " << length << ":\n";
+    for (int i = 0; i < length; i++) {
+        cin >> price[i];
     }
-    cout<<"Maximum Profit: "<<cutting_rod(price,length);
+
+    cout << "Maximum Profit: " << cutting_rod(price, length) << endl;
+    return 0;
 }
